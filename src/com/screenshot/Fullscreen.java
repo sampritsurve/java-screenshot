5f86c31d43a39001e00ccb4648030317e0872432
sampritsurve <58330543+sampritsurve@users.noreply.github.com>
2020-10-09 13:31:52 +0530

    first commit

---
+++ src/com/screenshot/Fullscreen.java
@@ -0,0 +1,57 @@
+package com.screenshot;
+
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+public class Fullscreen {
+	
+	static void takescreenshot(String url) throws IOException {
+		//Set a property of Chrome Webdriver, and specifying the path where the exe file of Chrome Driver is located  
+		System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
+		
+		//Create a driver object.
+		WebDriver driver  = new ChromeDriver();  
+		
+		//Open the url in the browser, specified by the user.
+		driver.get(url);
+		
+		//Maximize the Browser Window
+		driver.manage().window().maximize();
+		
+		//Create an AShot object, call the shootingStrategy() method to take screenshot of page bigger then size, then call a method takeScreenshot() passing the driver
+		Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
+		System.out.println("Screenshot captured");
+		
+		//Get the image from the screenshot and write it to the file. You can provide the file type as jpg, png, etc
+		//It causes IOException, so for handling exception we have to use a throws declaration or surround it with try catch block.
+		ImageIO.write(screenshot.getImage(), "jpg", new File("d:\\Fullscreenshot.jpg"));
+		
+		System.out.println("Screenshot written in file successfully..!!");
+
+	}
+
+	public static void main(String[] args) throws IOException{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//Take user input to take screenshot of a url
+		System.out.println("Enter the URL of a page to take a full screenshot: ");
+		String url =sc.nextLine();
+		sc.close();
+		
+		//Call the function takescreenshot and pass url
+		takescreenshot(url);	
+
+	}
+
+}
